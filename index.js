6bb41c5e1069adc9478643145c1b8d0db61ee1bd
Ric <jlsky519@163.com>
2018-01-11 09:00:07 +0800

    wrote a index file

---
+++ index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Hi from '@/components/Hi'
+import Hi1 from '@/components/Hi1'
+import Hi2 from '@/components/Hi2'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/hi/:aaa',
+      name: 'Hi',
+      component: Hi,
+      children:[
+	      	{
+	      		path: '/',
+	      		name: 'Hi'
+	      		
+	      	},
+	      {
+	      	path: 'hi1',
+	      	name: 'hi1',
+	      	component: Hi1
+	      },
+	      {
+	      	path: 'hi2/:aaa/:bbb',
+	      	name: 'Hi2',
+	      	component: Hi2
+	      }
+      ]
+    }
+  ]
+})
